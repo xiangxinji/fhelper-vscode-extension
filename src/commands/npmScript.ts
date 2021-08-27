cb2249a09c6f83316e1aa2b380abdb64a000c13c
xjxiang <xjxiang@ittx.com.cn>
2021-08-27 14:49:00 +0800

    添加 npm script 运行

---
+++ src/commands/npmScript.ts
@@ -0,0 +1,56 @@
+import path = require('path');
+import * as vscode from 'vscode';
+import * as fs from 'fs'
+
+const commandEntitys = {
+    runScript : 'extension.runScript' 
+}
+
+
+function getPackageJsonScripts (rootPath : string ) {
+    const p = path.join(rootPath, 'package.json')
+    try {
+        const file = fs.readFileSync(p ,'utf-8')
+        return JSON.parse(file).scripts 
+    } catch (error) {
+        return {}        
+    }
+}
+
+
+function createTerminal ( execute : string ) {
+    const terminal = vscode.window.createTerminal('run scripts')
+    terminal.sendText(execute , true )
+    terminal.show()
+}
+
+export default function (context : vscode.ExtensionContext) {
+    const command1 = vscode.commands.registerCommand(commandEntitys.runScript , () => {
+        let scripts:any = {} 
+        if(vscode.workspace.rootPath) {
+            scripts = getPackageJsonScripts(vscode.workspace.rootPath)
+        }
+        const scriptNames = Object.keys(scripts)
+        if(scriptNames.length === 0) {
+            return vscode.window.showInformationMessage('未找到package.json 或者 package.json 中未提供 script 字段')
+        }
+        // 创建 Pick 
+        const pick = vscode.window.createQuickPick()
+        pick.items = scriptNames.map(name => {
+            return { label :name , description : scripts[name] as string}
+        })
+        pick.onDidChangeSelection((selections) => {
+            if(selections.length > 0) {
+                const command = "npm run " + selections[0].label
+                createTerminal(command)
+                pick.hide()
+            }
+        })
+        pick.show()
+        pick.onDidHide(() => {
+            pick.dispose()
+        }) 
+
+    })
+    context.subscriptions.push(command1)
+}
